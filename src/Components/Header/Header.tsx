ae8d25aa10b81797d28b97e91fdf4dd36693334e
Vitalii Fedusov <vitalii.fedusov@gmail.com>
2024-02-12 22:16:12 +0200

    add base top-bar layout

--- src/Components/Header/Header.tsx
+++ src/Components/Header/Header.tsx
@@ -6,8 +6,8 @@ import arrowDown from '../../assets/icons/arrow-down.svg';
 
 export const Header: React.FC = () => {
   return (
-    <header className="header">
-      <div className="top-bar">
+    <header className="header page__header">
+      <div className="top-bar header__top-bar">
         <a href=".">
           <img src={logo} alt="CafeHub logo" />
         </a>
@@ -15,18 +15,30 @@ export const Header: React.FC = () => {
         <nav className="top-bar__nav">
           <ul className="top-bar__items">
             <li className="top-bar__item">
-              <div>
-                <img src={world} alt="icon world" />
-                <span>UA</span>
-                <img src={arrowDown} alt="arrow down" />
-              </div>
+              <img className="top-bar__icon" src={arrowDown} alt="arrow down" />
+              <span className="top-bar__icon">UA</span>
+              <img className="top-bar__icon" src={world} alt="icon world" />
             </li>
 
-            <li>
-              <button type="button">Вхід</button>
+            <li className="top-bar__item">
+              <button className="top-bar__button" type="button">Вхід</button>
             </li>
           </ul>
         </nav>
+      </div>
+
+      <div className="header__search-bar search-bar">
+        <ul className="search-bar__list">
+          <li className="search-bar__item search-bar__city">
+            <button type="button">Київ</button>
+          </li>
+          <li className="search-bar__item search-bar__input">
+            <input type="text" placeholder="Обери найкраще місце для свого відпочинку" />
+          </li>
+          <li className="search-bar__item search-bar__search">
+            <button type="button">Пошук</button>
+          </li>
+        </ul>
       </div>
     </header>
   );
--- src/Components/Main/Main.tsx
+++ src/Components/Main/Main.tsx
@@ -3,19 +3,7 @@ import React from 'react';
 export const Main: React.FC = () => {
   return (
     <main className="main">
-      <div className="search-bar">
-        <ul>
-          <li>
-            <button type="button">Київ</button>
-          </li>
-          <li>
-            <input type="text" placeholder="Обери найкраще місце для свого відпочинку" />
-          </li>
-          <li>
-            <button type="button">Пошук</button>
-          </li>
-        </ul>
-      </div>
+      main
     </main>
   );
 };
