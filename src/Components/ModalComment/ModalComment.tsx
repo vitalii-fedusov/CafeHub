d6500da1c1c3e1636b9a65d67a57baf2bdbd3b9c
Vitalii Fedusov <vitalii.fedusov@gmail.com>
2024-06-02 23:12:04 +0300

    fix linter error

--- src/Components/ModalComment/ModalComment.tsx
+++ src/Components/ModalComment/ModalComment.tsx
@@ -70,8 +70,8 @@ const TextareaElement = styled("textarea", {
   background: ${theme.palette.mode === "dark" ? grey[900] : "#fff"};
   border: 1px solid ${theme.palette.mode === "dark" ? grey[700] : grey[200]};
   box-shadow: 0px 2px 4px ${
-    theme.palette.mode === "dark" ? "rgba(0,0,0, 0.5)" : "rgba(0,0,0, 0.05)"
-  };
+  theme.palette.mode === "dark" ? "rgba(0,0,0, 0.5)" : "rgba(0,0,0, 0.05)"
+};
 
   &:hover {
     border-color: ${blue[400]};
